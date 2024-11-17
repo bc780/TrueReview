32b42193a7f5d9b0da057b5795e0bdca4ccd33a3
bc780 <124471008+bc780@users.noreply.github.com>
2024-11-17 15:22:45 -0500

    LSTM class

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader, random_split
+import torch.nn as nn
 
 bs = 16
+epochs = 10
 
 df = pd.read_csv("data/data.csv")
 
@@ -28,5 +30,27 @@ train_dataset, val_dataset, test_dataset = random_split(revDataset, [0.8*len(rev
 train_loader = DataLoader(train_dataset, batch_size = bs, shuffle = True)
 val_loader = DataLoader(val_dataset, batch_size = bs, shuffle = True)
 test_loader = DataLoader(test_dataset, batch_size = bs, shuffle = True)
+
+class revLSTM(nn.Module):
+    def __init__(self, vocab_size, embed_dim, hidden_dim, pad_idx = 0):
+        super(revLSTM, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size,embed_dim,padding_idx=pad_idx)
+        self.lstm = nn.LSTM(embed_dim, hidden_dim, batch_first = True)
+        self.fc = nn.Linear(hidden_dim, 1)
+
+        self.attention_weights = nn.Parameter(torch.randn(hidden_dim))
+
+    def attention(self, lstm_output):
+        score = torch.matmul(lstm_output)
+        weights = torch.softmax(score, dim=1)
+        weighted_output = lstm_output*weights
+        return weighted_output.sum(dim=1)
+    
+    def foward(self, x):
+        lstm_output, _ = self.lstm(self.embedding(x))
+        output = self.attention(lstm_output)
+        return torch.sigmoid(self.fc(output))
+
 
 
